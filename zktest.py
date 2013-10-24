11b934b951b9b0274ad795d8707364780d017868
Dony Wahyu Isp <dna.extrim@gmail.com>
2013-10-24 13:57:51 +0700

    edited by dnaextrim

--- zktest.py
+++ zktest.py
@@ -9,13 +9,13 @@ ret = zk.connect()
 print "Pesan Koneksi:", ret
 
 if ret == True:
-    #print "Pesan Versi:", zk.version()
-    #print "Pesan Versi OS:", zk.osversion()
+    print "Pesan Versi:", zk.version()
+    print "Pesan Versi OS:", zk.osversion()
     """
     print "Pesan Extend Format:", zk.extendFormat()
     print "Pesan Extend OP Log:", zk.extendOPLog()
     """
-    """
+    
     print "Pesan Platform:", zk.platform()
     print "Pesan Platform Version:", zk.fmVersion()
     print "Pesan Work Code:", zk.workCode()
@@ -25,7 +25,10 @@ if ret == True:
     print "Pesan Face Function On:", zk.faceFunctionOn()
     print "Pesan Serial Number:", zk.serialNumber()
     print "Pesan Device Name:", zk.deviceName()
-    """
-    print "Pesan Get User:", zk.getUser()
     
+    data_user = zk.getUser()
+    print "Pesan Get User:"
+    for uid in data_user
+        print "ID %s: %s" % ( uid, data_user[uid] )
+        
     print "Pesan Disconnect:", zk.disconnect()
