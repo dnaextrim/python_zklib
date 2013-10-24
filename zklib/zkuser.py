5edf8f69016bdf97849488d30ed709dcd479eed5
Dony Wahyu Isp <dna.extrim@gmail.com>
2013-10-24 14:02:55 +0700

    edited by dnaextrim

--- zklib/zkuser.py
+++ zklib/zkuser.py
@@ -27,46 +27,46 @@ def zkgetuser(self):
         reply_id, command_string)
     self.zkclient.sendto(buf, self.address)
     print buf.encode("hex")
-    #try:
-    self.data_recv, addr = self.zkclient.recvfrom(1024)
-    self.session_id = unpack('HHHH', self.data_recv[:8])[2]
-    
-    if getSizeUser(self):
-        bytes = getSizeUser(self)
-        while bytes > 0:
-            data_recv, addr = self.zkclient.recvfrom(1032)
-            self.userdata.append(data_recv)
-            bytes -= 1024
-            
-    data_recv = self.zkclient.recvfrom(8)
-    #print self.userdata
-    users = {}
-    if len(self.userdata) > 0:
-        # The first 4 bytes don't seem to be related to the user
-        for x in xrange(len(self.userdata)):
-            if x > 0:
-                self.userdata[x] = self.userdata[x][8:]
+    try:
+        self.data_recv, addr = self.zkclient.recvfrom(1024)
+        self.session_id = unpack('HHHH', self.data_recv[:8])[2]
         
-        userdata = ''.join( self.userdata )
+        if getSizeUser(self):
+            bytes = getSizeUser(self)
+            while bytes > 0:
+                data_recv, addr = self.zkclient.recvfrom(1032)
+                self.userdata.append(data_recv)
+                bytes -= 1024
+                
+        data_recv = self.zkclient.recvfrom(8)
         
-        userdata = userdata[23:]
-        
-        while (len(userdata) / 28) > 0:
+        users = {}
+        if len(self.userdata) > 0:
+            # The first 4 bytes don't seem to be related to the user
+            for x in xrange(len(self.userdata)):
+                if x > 0:
+                    self.userdata[x] = self.userdata[x][8:]
             
-            name, uid, prop = unpack( '28sx12s31s', userdata.ljust(72)[:72] )
+            userdata = ''.join( self.userdata )
             
-            # Clean up some messy characters from the user name
-            uid = unicode(uid.strip('\x00|\x01\x10x'), errors='ignore')
-            name = name.split('\x00', 1)[0]
+            userdata = userdata[23:]
             
-            if name.strip() == "":
-                name = uid
-            users[uid] = name
-            
-            #print("%s, %s" % (uid, name))
-            userdata = userdata[72:]
-            
-    return users
-    #except:
-        #return False
+            while (len(userdata) / 28) > 0:
+                
+                name, uid, prop = unpack( '28sx12s31s', userdata.ljust(72)[:72] )
+                
+                # Clean up some messy characters from the user name
+                uid = unicode(uid.strip('\x00|\x01\x10x'), errors='ignore')
+                name = name.split('\x00', 1)[0]
+                
+                if name.strip() == "":
+                    name = uid
+                users[uid] = name
+                
+                #print("%s, %s" % (uid, name))
+                userdata = userdata[72:]
+                
+        return users
+    except:
+        return False
     
