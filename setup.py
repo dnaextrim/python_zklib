562cea1d9b31307715ebb9a96ddac3ce59b91aa3
AlSayed Gamal <mail.gamal@gmail.com>
2015-06-28 22:51:49 +0200

    created python package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="zklib",
+    # Version number (initial):
+    version="0.1.0",
+    # Application author details:
+    url="http://pypi.python.org/pypi/python_zklib_v010/",
+    author="AlSayed Gamal",
+    author_email="gamal@vision-as.com",
+    packages=["zklib"],
+    license="LICENSE.txt",
+    description="Zk Attendance Machine lib",
+    long_description=open("README.txt").read(),
+
+)
